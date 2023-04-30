0608476d4e21f6640ba891a50674a785ee8e5a60
Leonid Solovev <ls3112@yandex.ru>
2023-04-30 17:04:37 +0300

    Task 27 done

---
+++ Task 27/Program.cs
@@ -0,0 +1,41 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+Console.Clear();
+
+int number = GetUserInput("Введите целое число: ", "Ошибка ввода!");
+
+int sum = GetSumOfNums(number);
+
+Console.WriteLine($"{number} -> {sum}");
+
+
+
+
+static int GetSumOfNums(int number)
+{
+    int i = 0;
+    int j;
+    while (number > 0)
+    {
+        j = number;
+        number = number / 10;
+        i = i + j % 10;
+    }
+    return i;
+}
+
+static int GetUserInput(string message, string errorMessage)
+{
+    while (true)
+    {
+        Console.Write(message);
+        if (int.TryParse(Console.ReadLine(), out int userNumber))
+            return userNumber;
+
+        Console.WriteLine(errorMessage);
+    }
+
+}
